f12d303278c1f63cfdfad36c371dbca848a030f8
Constantin Bargan <utopiaeh@UtopiaEH.local>
2024-02-20 15:36:38 +0200

    Added hasura alias

--- aliases.zsh
+++ aliases.zsh
@@ -3,3 +3,8 @@ alias zsource='source ~/.zshrc'
 alias cl='clear'
 alias lg='lazygit'
 alias oc='openconnect'
+
+alias h='hasura'
+alias hma='hasura migrate apply'
+alias hms='hasura migrate status'
+alias hc='hasura console'
